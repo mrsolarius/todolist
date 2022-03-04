93363be128ad7272bea8995c8ce8a4f0a5015d4c
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-04 09:43:14 +0100

    Feat : Mise en place de l'historique

---
+++ src/app/history.service.ts
@@ -0,0 +1,60 @@
+import {Inject, Injectable, InjectionToken} from '@angular/core';
+
+
+interface History<T> {
+  canUndo: boolean;
+  canRedo: boolean;
+  history: T[];
+  currentIndex: number;
+  current: T;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class HistoryService<T>{
+  private history: History<T>;
+
+  constructor(@Inject('HISTORY_SERVICE_PROVIDER') history: T) {
+    this.history = {
+      canUndo: false,
+      canRedo: false,
+      history: [history],
+      currentIndex: 0,
+      current: history
+    };
+  }
+
+  push(element: T) {
+    this.history.history.push(element);
+    this.history.currentIndex++;
+    this.history.canUndo = true;
+    this.history.canRedo = false;
+  }
+
+  undo() : T | null {
+    if (this.history.canUndo) {
+      this.history.currentIndex--;
+      this.history.current = this.history.history[this.history.currentIndex];
+      this.history.canRedo = true;
+      if (this.history.currentIndex === 0) {
+        this.history.canUndo = false;
+      }
+    }
+    return this.history.current;
+  }
+
+  redo() : T | null{
+    if (this.history.canRedo) {
+      this.history.currentIndex++;
+      this.history.current = this.history.history[this.history.currentIndex];
+      this.history.canUndo = true;
+      if (this.history.currentIndex === this.history.history.length - 1) {
+        this.history.canRedo = false;
+      }
+    }
+    return this.history.current;
+  }
+}
