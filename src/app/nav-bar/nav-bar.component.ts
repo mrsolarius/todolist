93bc3b351081f649b8b83dccc5c1df7a1ecb6cf8
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-03-18 11:58:28 +0100

    Feat : Firebase fonctionne

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(public auth: AngularFireAuth) { }
+
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
